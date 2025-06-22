531516def430330fdca40d136940da0f2ea2c8ef
okeysbytes <aoubah@gmail.com>
2025-06-22 11:58:13 +0100

    More initial changes to page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ import { lusitana } from '@/app/ui/fonts';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-10 shrink-0 items-end rounded-lg bg-red-500 p-4 md:h-26">
+      <div className="flex h-10 shrink-0 items-end bg-red-500 p-4 md:h-26">
       </div>
       
-      <div className="flex h-10 shrink-0 items-end rounded-lg bg-blue-500 md:h-26">
+      <div className="flex h-10 shrink-0 items-end bg-blue-500 md:h-26">
       </div>
       
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
@@ -18,7 +18,7 @@ export default function Page() {
             <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>We'll help you read and <strong><i>understand</i></strong> your <br/>first Java textbook</p>
             <Link
             href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-green-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
