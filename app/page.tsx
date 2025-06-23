d44e8717df8f6910735fff18030777a8a8979ec4
okeysbytes <aoubah@gmail.com>
2025-06-23 13:28:23 +0100

    Minor formatting changes

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Page() {
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <p className={`${lusitana.className} text-xl text-gray-
           800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Pretaught</strong></p>
+            <strong>Welcome to <i>pretaught.com</i></strong></p>
             <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>We will help you read and <strong><i>understand</i></strong> your <br/>first Java textbook</p>
             <Link
             href="/login"
