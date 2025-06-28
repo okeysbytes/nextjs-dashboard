39ebe6f2ccca67f4395a8ee233650f739d833b6f
okeysbytes <aoubah@gmail.com>
2025-06-28 15:05:15 +0100

    add 'currently' to existing phrase

--- app/explanations/page.tsx
+++ app/explanations/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <p className={`${lusitana.className} text-xl text-gray-
           800 md:text-3xl md:leading-normal`}>
-            <strong>This site is under construction</strong></p>
+            <strong>This site is currently under construction</strong></p>
             <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>Please bear with us!</p>
        </div>
        </div>
